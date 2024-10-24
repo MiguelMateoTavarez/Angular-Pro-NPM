1198fdc1946c0cef28cf75eb947cacad027d6666
Miguel Mateo <miguelmateot94@gmail.com>
2024-10-24 17:26:44 -0400

    feat: prepare admin layout and projects page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./modules/admin/layouts/AdminLayout/AdminLayout.component'),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./modules/admin/pages/Summary/Summary.component')
+      },
+      {
+        path: 'projects',
+        loadComponent: () => import('./modules/admin/pages/Projects/Projects.component')
+      },
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  }
+];
